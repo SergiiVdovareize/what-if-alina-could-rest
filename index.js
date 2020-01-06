53883cd0b8ac9dff10a01139861f2d77763cf07d
Sergey Vdovareize <servdo@gmail.com>
2020-01-06 23:37:09 +0200

    alis

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello Aliska bit.ly/35x5AQa\n');
 });
 server.listen(PORT, () => {
-    console.log(`Server running on ${PORT}/`);
-  });
+console.log(`Server running on ${PORT}/`);
+});

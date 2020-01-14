f1cbe68697004e0c6b6b192f003b71d234d7a7d4
Sergey Vdovareize <servdo@gmail.com>
2020-01-14 11:20:00 +0200

    config

--- config/app.config.js
+++ config/app.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    secret: 'kjbnKDglHB8723#_@342-_ed!Khjk3454jHGLDcfe-==34JHl'
+    secret: 'kjbnKDglHB8723#_@342-_ed!Khjk3454jHGLDcfe-==34JHl' // yes, secret must be an ENV variable instead of config file
 }
